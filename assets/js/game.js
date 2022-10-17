9199b226f02027c1fca1cd746ca1ca77bf2e9cc1
v1ct0r14m <tori.a.g.mota@gmail.com>
2022-10-17 03:03:22 -0500

    created name variables and fight function + notes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("this is an alert! javascript is running");
+var playerName = window.prompt("what is your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving a message");
+
+// this will do math and log 20
+console.log(10 + 10);
+
+// what is this?
+console.log("our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("THE FIGHT HAS BEGUN");
+}
+
+//fight();
